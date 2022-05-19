10bf79762096b1af7e8184b2f36804efec5f993e
Jarkko Jukarainen <jarkko.jukarainen@gmail.com>
2022-05-19 08:55:07 +0300

    Friend list duplicates

--- myfriendsduplicate.js
+++ myfriendsduplicate.js
@@ -1,1 +1,2 @@
+// Run at https://my.nianticlabs.com/friends to show duplicate friend entries
 [...document.querySelectorAll('.UIUserList_userInfo__1yQCA')].map(x => x.innerText).filter((v,i,s) => { return s.indexOf(v) !== i}).map(d => console.log(d));
